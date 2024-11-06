3233dcb6cafbbb345059711ba4710d6ca8bcc77d
Zun <k5orkrj@gmail.com>
2024-11-06 19:59:47 +0900

    setup: html and css for header and footer

---
+++ src/main/java/inflearn/testthymeleaf/TestController.java
@@ -0,0 +1,13 @@
+package inflearn.testthymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping()
+    public String test() {
+        return "index";
+    }
+}
